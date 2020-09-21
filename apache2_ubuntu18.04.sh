d4265c12881c9fcad9cc862b9c17798a5c87d517
Technoknol <technoknol@users.noreply.github.com>
2020-09-21 15:37:38 +0530

    Update apache2_ubuntu18.04.sh

--- apache2_ubuntu18.04.sh
+++ apache2_ubuntu18.04.sh
@@ -3,3 +3,8 @@ sudo apt update
 sudo apt install -y apache2
 sudo systemctl status apache2
 hostname -I
+sudo a2enmod rewrite
+sudo a2enmod proxy
+sudo a2enmod prody_http
+sudo a2enmod ssl
+sudo a2enmod proxy_balancer

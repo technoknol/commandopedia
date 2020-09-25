6f894c2717a4b78dec83b49e5e5076c3d745dc87
Technoknol <technoknol@users.noreply.github.com>
2020-09-25 12:04:19 +0530

    Update certbot_nginx_ubuntu18.04.sh

--- certbot_nginx_ubuntu18.04.sh
+++ certbot_nginx_ubuntu18.04.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
 sudo apt install -y python-certbot-nginx
 
 # Obtain Certificate command
 # sudo certbot --apache -d your_domain -d www.your_domain
 echo "Run following command to obtain a certificate."
 echo "Replace your_domain with your domain."
-echo "sudo certbot --apache -d your_domain -d www.your_domain"
+echo "sudo certbot --nginx -d your_domain -d www.your_domain"

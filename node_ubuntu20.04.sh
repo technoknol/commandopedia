99cea1381fdada26b361686e7fe15fbdfb83467a
Technoknol <technoknol@users.noreply.github.com>
2021-01-26 19:36:05 +0530

    Update node_ubuntu20.04.sh

--- node_ubuntu20.04.sh
+++ node_ubuntu20.04.sh
@@ -3,9 +3,9 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
-echo 'nodejs -v'
-nodejs -v
+echo 'node -v'
+node -v
 echo 'npm -v'
 npm -v
-sudo apt install build-essential
+sudo apt install build-essential -y
 
